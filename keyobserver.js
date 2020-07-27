9dc0075f965aea628839eed18f873ec07c871851
manolo <manuel.gutierrez.arias2010@gmail.com>
2020-07-26 19:02:52 -0500

    Add mobile support

--- keyobserver.js
+++ keyobserver.js
@@ -19,6 +19,7 @@ Window.keyEventsObserver = (function(){
   };
 
   function refreshPage(){
+    console.log("refreshing")    
     location.reload(true);
   };
 
@@ -36,8 +37,9 @@ Window.keyEventsObserver = (function(){
 
   var setKeyEvents = function(){
     document.onkeydown = function(event) {
-      var keyCode = (event == null ? window.event.keyCode : event.keyCode);
+      var keyCode = (event == null ? window.event.keyCode : event.keyCode);      
 
+      console.log("here")
       switch(keyCode){
         case 37:
           processKeyPress('left');
@@ -54,7 +56,7 @@ Window.keyEventsObserver = (function(){
         case 13:
           refreshPage();
           break;
-        case 32:
+        case 32:          
           pauseTheWholeThing();
           break;
         default:
@@ -63,9 +65,68 @@ Window.keyEventsObserver = (function(){
     }
   };
 
+
+  var setGestureEvent = function(){
+    //find a better place for this call, I dont like it :( 
+    //Make mobile work    
+    $("#start_game_button").click(function() {
+      console.log("Restarting the game");
+      refreshPage();
+    });
+
+    var el = document.getElementById("canvas");
+
+    el.addEventListener('touchstart', handleTouchStart, false);        
+    el.addEventListener('touchmove', handleTouchMove, false);
+
+    var xDown = null;                                                        
+    var yDown = null;                                                        
+
+    function handleTouchStart(evt) {        
+      //xDown = evt.originalEvent.touches[0].clientX;   
+      xDown = evt.touches[0].clientX;                                                                         
+      yDown = evt.touches[0].clientY;                                      
+    };                                                
+
+    function handleTouchMove(evt) {
+      if ( ! xDown || ! yDown ) {
+        return;
+      }
+
+      var xUp = evt.touches[0].clientX;                                    
+      var yUp = evt.touches[0].clientY;
+
+      var xDiff = xDown - xUp;
+      var yDiff = yDown - yUp;
+
+      if ( Math.abs( xDiff ) > Math.abs( yDiff ) ) {/*most significant*/
+        if ( xDiff > 0 ) {
+            /* left swipe */ 
+            processKeyPress('left');
+        } else {
+            /* right swipe */
+            processKeyPress('right');
+        }                       
+      } else {
+        if ( yDiff > 0 ) {
+            /* up swipe */ 
+            processKeyPress('up');
+        } else { 
+            /* down swipe */
+            processKeyPress('down');
+        }                                                                 
+      }
+      /* reset values */
+      xDown = null;
+      yDown = null;                                             
+      };
+  };
+
   return {
     init: function(){
       setKeyEvents();
+      //Only if mobile
+      setGestureEvent();
     },
     register: function(obj){
       register(obj)
